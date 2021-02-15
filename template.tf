b3307d881a0d888bb70ea6ea6614204329d2a3d6
jrzeszutek <jaroslaw@cloudify.co>
2021-02-15 14:33:08 +0100

    minor container fix

--- template.tf
+++ template.tf
@@ -35,7 +35,7 @@ resource "azurerm_storage_account" "blob" {
 }
 
 resource "azurerm_storage_container" "blob" {
-  name                  = format("%s/%s", var.blob_name, "-container")
+  name                  = format("%s%s", var.blob_name, "-container")
   storage_account_name  = azurerm_storage_account.blob.name
   container_access_type = "private"
 }
